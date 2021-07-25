a3371f31b473941779091ffb4971379007305d9e
markgacoka <markgacoka@gmail.com>
2021-07-26 01:57:48 +0300

    Add dashboard to Django project

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('checkout/', views.checkout, name='checkout'),
+
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('forgot-password/', views.forgot_password, name='forgot_password'),
+
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
