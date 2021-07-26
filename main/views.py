f4aa3cee9a546f7bacbbeef414f38de50afdc4e7
markgacoka <markgacoka@gmail.com>
2021-07-26 04:58:37 +0300

    Add active highlighting to sidebar, extend base html to all dashboard pages

--- main/views.py
+++ main/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-def privacy_policy(request):
-    return render(request, 'privacy-policy.html')
-
-def terms_conditions(request):
-    return render(request, 'terms-conditions.html')
-
 def checkout(request):
     return render(request, 'pages/checkout.html')
+
+def privacy_policy(request):
+    return render(request, 'pages/privacy-policy.html')
+
+def terms_conditions(request):
+    return render(request, 'pages/terms-conditions.html')
 
 def login(request):
     return render(request, 'auth/login.html')
@@ -22,4 +22,37 @@ def forgot_password(request):
     return render(request, 'auth/forgot-password.html')
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'dashboard/dashboard.html')
+
+def projects(request):
+    return render(request, 'dashboard/projects.html')
+
+def notes(request):
+    return render(request, 'dashboard/notes.html')
+
+def subdomain_enum(request):
+    return render(request, 'dashboard/subdomain_enum.html')
+
+def directory_enum(request):
+    return render(request, 'dashboard/directory_enum.html')
+
+def vuln_analysis(request):
+    return render(request, 'dashboard/vuln_analysis.html')
+    
+def dorking(request):
+    return render(request, 'dashboard/dorking.html')
+
+def exploit(request):
+    return render(request, 'dashboard/exploits.html')
+
+def req_tamperer(request):
+    return render(request, 'dashboard/req_tamperer.html')
+
+def wordlist_gen(request):
+    return render(request, 'dashboard/wordlist_gen.html')
+
+def decoder(request):
+    return render(request, 'dashboard/decoder.html')
+
+def file_upload(request):
+    return render(request, 'dashboard/file_upload.html')
