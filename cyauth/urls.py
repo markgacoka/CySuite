38af1592dc614c2ecfbb1c84d225a89681ed9e12
markgacoka <markgacoka@gmail.com>
2021-07-31 05:37:23 +0300

    Fix password reset form

--- cyauth/urls.py
+++ cyauth/urls.py
@@ -18,14 +18,8 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name="dashboard"),
     path('profile/', account_view, name="profile"),
 
-    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='auth/password_change_done.html'), 
-        name='password_change_done'),
-    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='auth/password_change.html'), 
-        name='password_change'),
-    path('password-reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_done.html'),
-        name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='auth/password_reset.html'), name='password_reset_confirm'),
     path('reset-password/', auth_views.PasswordResetView.as_view(template_name='auth/forgot-password.html'), name='password_reset'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_complete.html'),
-        name='password_reset_complete'),
+    path('password-reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='auth/password_reset.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_complete.html'), name='password_reset_complete'),
 ]
