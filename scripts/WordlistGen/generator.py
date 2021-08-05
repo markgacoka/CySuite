bab93e727b34dc3d1336b9708e14ff35c5f0383d
markgacoka <markgacoka@gmail.com>
2021-08-05 06:45:28 +0300

    Finish wordlist generator script

---
+++ scripts/WordlistGen/generator.py
@@ -0,0 +1,11 @@
+import requests, re
+import urllib.request
+from bs4 import BeautifulSoup
+
+def extract_wordlist(url):
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, features="html.parser")
+    text = soup.get_text().split()
+    wordlist = set(filter(lambda x:x, map(lambda x:re.sub(r'[^A-Za-z]', '', x), text)))
+
+    return wordlist
