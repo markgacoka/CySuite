7cb8a6757c45c46ddb28588f2e21fdafe79705d9
markgacoka <markgacoka@gmail.com>
2021-08-15 21:38:46 +0300

    Delete account from database

--- cyauth/signals.py
+++ cyauth/signals.py
@@ -2,13 +2,16 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Account
 from .models import UserProfile
+from main.models import PayloadModel
 
 
 @receiver(post_save, sender=Account) 
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        UserProfile.objects.create(username=instance)
+        PayloadModel.objects.create(payload_user=instance)
 
 @receiver(post_save, sender=Account)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+    instance.payload.save()
--- main/views.py
+++ main/views.py
@@ -257,7 +257,8 @@ def file_upload(request):
             elif filename != None or dimensions != None:
                 new_filename = re.sub(r'^.*?/', '', filename)
                 payload_file = PayloadModel.objects.get(payload_user=request.user)
-                if new_filename != payload_file.payload_image.path:
+                print(payload_file.payload_image.path)
+                if new_filename != payload_file.payload_image.path or not(payload_file.payload_image != 'default.png'):
                     os.remove(payload_file.payload_image.path)
                 payload_file.payload_image = new_filename
                 payload_file.save()
