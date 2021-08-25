7f9a1e07254709baacc450be4bcdaa93d60c0c4d
markgacoka <markgacoka@gmail.com>
2021-08-26 02:47:12 +0300

    Add async background subdomain enum to frontend with progress bar

--- scripts/Requests/status_code.py
+++ scripts/Requests/status_code.py
@@ -1,17 +1,23 @@
 import socket
+import requests
 import http.client as httplib
-from http.client import responses
+from http.client import responses, InvalidURL
 from urllib.error import HTTPError, URLError
 from urllib.request import Request, urlopen
 
 def status_code(domain):
     try:
+        if domain.strip() == '':
+            status = 'Not Applicable'
+            yield status
         if not domain.startswith('http://') and not domain.startswith('https://'):
             domain = 'http://' + domain
         req = Request(domain, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'})
         code = urlopen(req, timeout=10).getcode()
     except HTTPError as e:
         status = str(e.code) + ' ' + httplib.responses[e.code]
+    except InvalidURL:
+        status = 'Not Applicable'
     except URLError as e:
         status = 'Not Applicable'
     except socket.timeout as e:
