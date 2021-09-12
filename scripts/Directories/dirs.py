378a30d12962c89fad3561aeecf7b1a3628c364e
markgacoka <markgacoka@gmail.com>
2021-09-12 03:16:18 +0200

    Clean up crawling script to correct output

--- scripts/Directories/dirs.py
+++ scripts/Directories/dirs.py
@@ -1,21 +1,4 @@
 # Crawling, Passive sources (wayback), Databases (threat intelligence and web scrapers)
- 
-# from bs4 import BeautifulSoup
-# from urllib.request import Request, urlopen
-
-# def scrape_links(domain):
-#     req = Request(domain, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'})
-#     html_page = urlopen(req)
-#     soup = BeautifulSoup(html_page, "lxml")
-
-#     links = []
-#     for link in soup.findAll('a'):
-#         links.append(link.get('href'))
-
-#     return(links)
-
-# url = 'https://markgacoka.com'
-# print(scrape_links(url))
 
 import requests
 from urllib.parse import urlparse, urljoin
@@ -39,7 +22,7 @@ def get_all_website_links(url):
     urls = set()
     # domain name of the URL without the protocol
     domain_name = urlparse(url).netloc
-    soup = BeautifulSoup(requests.get(url).content, "html.parser")
+    soup = BeautifulSoup(requests.get(url).content, "html.parser", from_encoding="iso-8859-1")
     for a_tag in soup.findAll("a"):
         href = a_tag.attrs.get("href")
         if href == "" or href is None:
@@ -54,10 +37,8 @@ def get_all_website_links(url):
             continue
         if domain_name not in href:
             if href not in external_urls:
-                print(f"[!] External link: {href}")
                 external_urls.add(href)
             continue
-        print(f"[*] Internal link: {href}")
         urls.add(href)
         internal_urls.add(href)
     return urls
@@ -72,16 +53,16 @@ def crawl(url, max_urls=30):
     """
     global total_urls_visited
     total_urls_visited += 1
-    print(f"[*] Crawling: {url}")
     links = get_all_website_links(url)
     for link in links:
         if total_urls_visited > max_urls:
             break
         crawl(link, max_urls=max_urls)
 
-if __name__ == "__main__":
-    url = 'https://markgacoka.com'
+def main(url):
     crawl(url)
-    print("[+] Total Internal links:", len(internal_urls))
-    print("[+] Total External links:", len(external_urls))
-    print("[+] Total URLs:", len(external_urls) + len(internal_urls))
+    return [len(external_urls) + len(internal_urls), internal_urls, external_urls]
+
+url = 'https://markgacoka.com'
+# [total number of links, [internal_links], [external_links]]
+print(main(url))
