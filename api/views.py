0fa20610fdfc9d6adce6a13a1ba65b5e1245aaa8
markgacoka <markgacoka@gmail.com>
2021-10-10 18:40:15 +0200

    Change profile image to correct init link

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+def home(request):
+    response = json.dumps([{}])
+    return HttpResponse(response, content_type='text/json')
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'username': 'admin',
+            'years_active': 10
+        }
+        return Response(data)
